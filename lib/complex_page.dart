0f718fe44672706dc45808146581bcffafff25b5
bladeofgods <bladeofgods@163.com>
2020-07-24 17:48:41 +0800

    骚操作

--- lib/complex_page.dart
+++ lib/complex_page.dart
@@ -7,8 +7,13 @@
 
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+//import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
 
 class ComplexPage extends StatefulWidget{
   @override
@@ -31,20 +36,67 @@ class ComplexPageState extends State<ComplexPage> {
   }
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Material(
-      color: Colors.white,
+      color: Colors.deepPurple,
+      child: Container(
+        padding: EdgeInsets.all(50),
+        width: size.width,height: size.height,
+        child:justWebP(size),
+
+      ),
+    );
+  }
+
+  Widget normal(Size size){
+    return SingleChildScrollView(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          Container(
+            width: size.width,height: size.height/2,
+            color: Colors.lightBlue,
+          ),
           Text('complex page'),
+//            Container(
+//              padding: EdgeInsets.all(10),
+//              width: size.width,height: size.height,
+//              color: Colors.red,
+//              child: WebView(
+//                initialUrl: "https://www.tripalink.com",
+//                javascriptMode: JavascriptMode.unrestricted,
+//              ),
+//            ),
+//          WebView(
+//            initialUrl: "https://www.tripalink.com",
+//            javascriptMode: JavascriptMode.unrestricted,
+//          ),
           Container(
             width: 200,height: 200,
             color: Colors.greenAccent,
             alignment: Alignment.center,
             child: getAndroidTextView(),
           ),
+          Container(
+            width: size.width,height: size.height/2,
+            color: Colors.lightBlue,
+          ),
         ],
       ),
+    );
+  }
+
+  final url = "https://www.tripalink.com";
+//  Widget justWeb(Size size){
+//    return WebView(
+//      initialUrl: url,
+//      javascriptMode: JavascriptMode.unrestricted,
+//    );
+//  }
+  Widget justWebP(Size size){
+    return WebviewScaffold(
+      url: url,
+      withJavascript: true,
     );
   }
 }
